757d694ff33e53772768a3ea4145e6daaec129bb
moduaiffel <moduaiffel@gmail.com>
2021-04-14 01:09:37 +0900

    mysql environment moded

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const pool = mysql.createPool({
-        connectionLimit: 10,
-        host: 'mysql',
-        user: 'root',
-        password: 'johnahn',
-        database: 'myapp'
+    connectionLimit: 10,
+//     host: 'mysql',
+//     user: 'root',
+//     password: 'jojnahn',
+//     database: 'myapp',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
-
 exports.pool = pool;
-
-
